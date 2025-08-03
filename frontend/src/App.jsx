4ac101b7092ea793d270710307e96b133f3ee1c5
Mohamed Hassan <mohassan1322006@gmail.com>
2025-08-03 06:08:24 +0300

    start-react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// عدل المسار حسب مكان الملف
+
+export default function App() {
+  return (
+    <div>
+      <h1>ICP Greeting</h1>
+    </div>
+  );
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,36 +1,43 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'url';
-import environment from 'vite-plugin-environment';
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import environment from "vite-plugin-environment";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
-  plugins: [environment('all', { prefix: 'CANISTER_' }), environment('all', { prefix: 'DFX_' })],
-  envDir: '../',
+  base: "./",
+  plugins: [
+    react(),
+    environment("all", { prefix: "CANISTER_" }),
+    environment("all", { prefix: "DFX_" }),
+  ],
+  envDir: "../",
   define: {
-    'process.env': process.env
+    "process.env": process.env,
   },
   optimizeDeps: {
     esbuildOptions: {
       define: {
-        global: 'globalThis'
-      }
-    }
+        global: "globalThis",
+      },
+    },
   },
   resolve: {
     alias: [
       {
-        find: 'declarations',
-        replacement: fileURLToPath(new URL('../src/declarations', import.meta.url))
-      }
-    ]
+        find: "declarations",
+        replacement: fileURLToPath(
+          new URL("../src/declarations", import.meta.url)
+        ),
+      },
+    ],
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4943',
-        changeOrigin: true
-      }
+      "/api": {
+        target: "http://127.0.0.1:4943",
+        changeOrigin: true,
+      },
     },
-    host: '127.0.0.1'
-  }
+    host: "127.0.0.1",
+  },
 });
